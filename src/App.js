33f7b77f213f4ef93afb6de40ffb99f6849ff7c3
nirav <niravchauhan9027@gmail.com>
2025-06-25 23:16:06 +0530

    setup completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Routing from "./routes/Routing";
 import './assets/scss/svgStyle.scss';
 import './assets/scss/global.scss';
 import { Suspense } from "react";
+import Loader from "./components/Loader";
 
 function App() {
   return (
